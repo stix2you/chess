41ee8d8c19ea53d838d03c749ac8172b36a4d5ff
stix2you <stix2you@gmail.com>
2024-05-06 22:04:56 -0500

    init repo - board rendered

---
+++ src/components/Chessboard.tsx
@@ -0,0 +1,23 @@
+import react from 'react';
+import './Chessboard.css';
+
+const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+
+export default function Chessboard() {
+   let board = [];
+   for (let j = verticalAxis.length - 1; j >= 0; j--) {
+      for (let i = 0; i < horizontalAxis.length; i++) {
+         const number = j + i + 2;
+       if (number % 2 === 0) {
+         board.push(<div className="tile black-tile"></div>);
+         } else {
+         board.push(<div className="tile white-tile"></div>);
+         }        
+      }
+   }
+
+   return (
+      <div id="chessboard">{board}</div>);
+
+}
