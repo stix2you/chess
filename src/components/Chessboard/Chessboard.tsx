d354423f0063acb0e077cb86b02ff9b0b66596f0
stix2you <stix2you@gmail.com>
2024-05-07 18:47:13 -0500

    implement [ieces moving and snapping to a square -- also better state management of coordinates and where active piece is

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Tile from '../Tile/Tile';
 import './Chessboard.css';
 
@@ -11,48 +11,67 @@ interface Piece {
    y: number;
 }
 
-const pieces: Piece[] = [];
+const initialBoardState: Piece[] = [];
 
+// Initialize the pieces state with the initial positions of all chess pieces
+initialBoardState.push({ image: "./assets/img/rook_white.png", x: 0, y: 0 });
+initialBoardState.push({ image: "./assets/img/rook_white.png", x: 7, y: 0 });
+initialBoardState.push({ image: "./assets/img/rook_black.png", x: 0, y: 7 });
+initialBoardState.push({ image: "./assets/img/rook_black.png", x: 7, y: 7 });
+initialBoardState.push({ image: "./assets/img/knight_white.png", x: 1, y: 0 });
+initialBoardState.push({ image: "./assets/img/knight_white.png", x: 6, y: 0 });
+initialBoardState.push({ image: "./assets/img/knight_black.png", x: 1, y: 7 });
+initialBoardState.push({ image: "./assets/img/knight_black.png", x: 6, y: 7 });
+initialBoardState.push({ image: "./assets/img/bishop_white.png", x: 2, y: 0 });
+initialBoardState.push({ image: "./assets/img/bishop_white.png", x: 5, y: 0 });
+initialBoardState.push({ image: "./assets/img/bishop_black.png", x: 2, y: 7 });
+initialBoardState.push({ image: "./assets/img/bishop_black.png", x: 5, y: 7 });
+initialBoardState.push({ image: "./assets/img/queen_white.png", x: 3, y: 0 });
+initialBoardState.push({ image: "./assets/img/queen_black.png", x: 3, y: 7 });
+initialBoardState.push({ image: "./assets/img/king_white.png", x: 4, y: 0 });
+initialBoardState.push({ image: "./assets/img/king_black.png", x: 4, y: 7 });
 for (let i = 0; i < 8; i++) {
-   pieces.push({ image: "./assets/img/pawn_white.png", x: i, y: 1 });
-   pieces.push({ image: "./assets/img/pawn_black.png", x: i, y: 6 });
+   initialBoardState.push({ image: "./assets/img/pawn_white.png", x: i, y: 1 });
+   initialBoardState.push({ image: "./assets/img/pawn_black.png", x: i, y: 6 });
 }
 
-pieces.push({ image: "./assets/img/rook_white.png", x: 0, y: 0 });
-pieces.push({ image: "./assets/img/rook_white.png", x: 7, y: 0 });
-pieces.push({ image: "./assets/img/rook_black.png", x: 0, y: 7 });
-pieces.push({ image: "./assets/img/rook_black.png", x: 7, y: 7 });
-pieces.push({ image: "./assets/img/knight_white.png", x: 1, y: 0 });
-pieces.push({ image: "./assets/img/knight_white.png", x: 6, y: 0 });
-pieces.push({ image: "./assets/img/knight_black.png", x: 1, y: 7 });
-pieces.push({ image: "./assets/img/knight_black.png", x: 6, y: 7 });
-pieces.push({ image: "./assets/img/bishop_white.png", x: 2, y: 0 });
-pieces.push({ image: "./assets/img/bishop_white.png", x: 5, y: 0 });
-pieces.push({ image: "./assets/img/bishop_black.png", x: 2, y: 7 });
-pieces.push({ image: "./assets/img/bishop_black.png", x: 5, y: 7 });
-pieces.push({ image: "./assets/img/queen_white.png", x: 3, y: 0 });
-pieces.push({ image: "./assets/img/queen_black.png", x: 3, y: 7 });
-pieces.push({ image: "./assets/img/king_white.png", x: 4, y: 0 });
-pieces.push({ image: "./assets/img/king_black.png", x: 4, y: 7 });
-
-
-
-
 export default function Chessboard() {
+   const [activePiece, setActivePiece] = useState<HTMLElement | null>(null);  // Initialize the activePiece state with null
+   const [gridX, setGridX] = useState<number>(0);
+   const [gridY, setGridY] = useState<number>(0);
+   const [pieces, setPieces] = useState<Piece[]>(initialBoardState);  // Initialize the pieces state with an empty array
    const chessboardRef = useRef<HTMLDivElement>(null);  // Create a reference to the chessboard div element
    let board = [];  // The board array will contain the Tile components
-   let activePiece: HTMLElement | null = null;  // The activePiece variable will store the piece that is being dragged
 
    // Grab the piece that is being clicked
    function grabPiece(e: React.MouseEvent) {
       const element = e.target as HTMLElement;
-      if (element.classList.contains('chess-piece')) {
+      const chessboard = chessboardRef.current;  // Get the chessboard element
+      if (element.classList.contains('chess-piece') && chessboard) {
+
+         // Calculate the raw x and y coordinates based on the mouse event relative to the chessboard
+         const rawX = (e.clientX - chessboard.offsetLeft) / 90;
+         const rawY = (720 - e.clientY - chessboard.offsetTop) / 90; // subtract the y-coordinate from 720 to invert the y-axis
+         // Round x and y down to get the nearest lower integer
+         const flooredX = Math.floor(rawX);
+         const flooredY = Math.floor(rawY);
+         // Ensure x and y coordinates are within the 0 to 7 range, and do not exceed the chessboard boundaries
+         const gridX = Math.max(0, Math.min(7, flooredX));
+         const gridY = Math.max(0, Math.min(7, flooredY));
+
+         setGridX(gridX);  // Update the gridX state with the x-coordinate of the piece (sets the global state of the x-coordinate)
+         setGridY(gridY);  // Update the gridY state with the y-coordinate of the piece (sets the global state of the y-coordinate)
+         
+         console.log("on grab:", gridX, gridY);
+
          const x = e.clientX - 45;
          const y = e.clientY - 45;
+         
          element.style.position = 'absolute';
          element.style.left = `${x}px`;
          element.style.top = `${y}px`;
-         activePiece = element;
+         
+         setActivePiece(element);  // Set the activePiece state to the clicked element
       }
    }
 
@@ -87,24 +106,50 @@ export default function Chessboard() {
    }
 
    function dropPiece(e: React.MouseEvent) {
-      if (activePiece) {
-         activePiece = null;
+      const chessboard = chessboardRef.current;  // Get the chessboard element
+      if (activePiece && chessboard) {   // Check if there is an active piece
+         // Calculate the raw x and y coordinates based on the mouse event relative to the chessboard
+         const rawX = (e.clientX - chessboard.offsetLeft) / 90;
+         const rawY = (720 - e.clientY - chessboard.offsetTop) / 90; // subtract the y-coordinate from 720 to invert the y-axis
+
+         // Round x and y down to get the nearest lower integer
+         const flooredX = Math.floor(rawX);
+         const flooredY = Math.floor(rawY);
+
+         // Ensure x and y coordinates are within the 0 to 7 range, and do not exceed the chessboard boundaries
+         const x = Math.max(0, Math.min(7, flooredX));
+         const y = Math.max(0, Math.min(7, flooredY));
+
+         console.log("on drop:", x, y);
+
+         setPieces((value) => {     // Update the pieces state with the new position of the piece
+            const pieces = value.map(p => {    // Loop through the pieces array
+               if (p.x === gridX && p.y === gridY) {  // Check if the piece is on the current tile
+                  p.x = x;
+                  p.y = y;
+               }
+               return p;              // Return the piece as it is
+            })
+            return pieces;  // Return the updated pieces array
+         });
+         
+         setActivePiece(null);  // Reset the activePiece state to null
       }
    }
 
    // Create the chessboard
-   for (let j = verticalAxis.length - 1; j >= 0; j--) {
-      for (let i = 0; i < horizontalAxis.length; i++) {
-         const number = j + i + 2;
-         let image = '';
+   for (let j = verticalAxis.length - 1; j >= 0; j--) {  // Loop through the vertical axis in reverse order
+      for (let i = 0; i < horizontalAxis.length; i++) {  // Loop through the horizontal axis for each vertical tile
+         const number = j + i + 2;                       // Calculate the number of the tiles
+         let image = '';                                 // Initialize the image variable to an empty string
 
-         pieces.forEach(piece => {
-            if (piece.x === i && piece.y === j) {
-               image = piece.image;
+         pieces.forEach(piece => {                    // Loop through the pieces array       
+            if (piece.x === i && piece.y === j) {     // Check if the piece is on the current tile
+               image = piece.image;                   // Set the image variable to the image of the piece
             }
          });
 
-         board.push(<Tile key={`${j},${i}`} image={image} number={number} />);
+         board.push(<Tile key={`${j},${i}`} image={image} number={number} />);  // Add the Tile component to the board array
          // Add the key prop to the Tile component, the key prop is a unique identifier for each Tile component
       }
    }
